e84edbfc81ebd11652445ce774eecc91b52674c5
klamparski <klamparski@gmail.com>
2019-03-15 14:33:22 +0100

    Initialize extension using extension builder

---
+++ ext_tables.php
@@ -0,0 +1,11 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(
+    function()
+    {
+
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('restify', 'Configuration/TypoScript', 'Restify');
+
+    }
+);
