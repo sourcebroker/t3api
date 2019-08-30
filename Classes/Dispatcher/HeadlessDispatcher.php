8e7fb5d604a998f45925390dc96e9c55bda22cd4
Lukasz Dobrowolski <l00k+pro@protonmail.com>
2019-08-30 17:05:11 +0200

    TASK Cleanup headless dispatcher

--- Classes/Dispatcher/HeadlessDispatcher.php
+++ Classes/Dispatcher/HeadlessDispatcher.php
@@ -2,29 +2,11 @@
 
 namespace V\Local\Service;
 
-use Exception;
 use SourceBroker\T3api\Dispatcher\AbstractDispatcher;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RequestContext;
-use TYPO3\CMS\Core\Routing\RouteNotFoundException;
 use TYPO3\CMS\Core\SingletonInterface;
 
 class HeadlessDispatcher
     extends AbstractDispatcher
     implements SingletonInterface
 {
-
-    /**
-     * @return string
-     * @throws Exception
-     * @throws RouteNotFoundException
-     */
-    public function process(string $route): string
-    {
-        $context = new RequestContext();
-        $context->setPathInfo($route);
-
-        return $this->processRequestByContext($context);
-    }
-
 }
