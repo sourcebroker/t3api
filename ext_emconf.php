e06234e94a155123561a22762c5402d72fb4cbfc
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2019-09-13 14:49:00 +0200

    [TASK] Update description for TER.

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'T3api',
-    'description' => 'REST API',
+    'description' => 'REST API for your TYPO3 project. Config with annotations, build in filtering, pagination, typolinks, image processing, serialization contexts, responses in Hydra/JSON-LD format.',
     'category' => 'plugin',
     'author' => 'SourceBroker Team',
     'author_email' => 'office@sourcebroker.dev',
