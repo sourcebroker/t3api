d9131df1725ff4166085ad0a63843c17f3a0c586
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2019-09-13 15:52:07 +0200

    [TASK] Start version for unit tests.

---
+++ Tests/Unit/Domain/Model/ApiFilterTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace SourceBroker\T3api\Tests\Unit\Domain\Model;
+
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+/**
+ * Test case.
+ */
+class ApiFilterTest extends UnitTestCase
+{
+    /**
+     * @var \SourceBroker\T3api\Domain\Model\ApiFilter
+     */
+    protected $subject = null;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->subject = new \SourceBroker\T3api\Domain\Model\ApiFilter('filterClass', 'property', 'strategy', ['argument' => 'argumentValue']);
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function getFilterClassReturnsInitialValueForString()
+    {
+        self::assertSame(
+            'filterClass',
+            $this->subject->getFilterClass()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getStrategyReturnsInitialValueForString()
+    {
+        self::assertSame(
+            'strategy',
+            $this->subject->getStrategy()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getPropertyReturnsInitialValueForString()
+    {
+        self::assertSame(
+            'property',
+            $this->subject->getProperty()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getArgumentsReturnsInitialValueForArray()
+    {
+        self::assertSame(
+            ['argument' => 'argumentValue'],
+            $this->subject->getArguments()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function getArgumentReturnsInitialValueForAccessingSingleItem()
+    {
+        self::assertSame(
+            'argumentValue',
+            $this->subject->getArgument('argument')
+        );
+    }
+}
