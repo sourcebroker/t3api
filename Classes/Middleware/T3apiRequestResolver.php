d9b0df2643011b2d8e4586034a346be485e6bd82
Karol Lamparski <klamparski@gmail.com>
2020-04-14 08:56:27 +0200

    Fixes wrongly included parameter `t3apiResource` in pagination URLs in hydra collection response

--- Classes/Middleware/T3apiRequestResolver.php
+++ Classes/Middleware/T3apiRequestResolver.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use SourceBroker\T3api\Dispatcher\Bootstrap;
-use TYPO3\CMS\Core\Routing\RouteNotFoundException;
+use Throwable;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 
@@ -21,17 +21,31 @@ class T3apiRequestResolver implements MiddlewareInterface
      * @param ServerRequestInterface $request
      * @param RequestHandlerInterface $handler
      *
-     * @throws RouteNotFoundException
      * @return ResponseInterface
+     * @throws Throwable
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if (is_array($request->getQueryParams()) && array_key_exists('t3apiResource', $request->getQueryParams())) {
             return GeneralUtility::makeInstance(ObjectManager::class)
                 ->get(Bootstrap::class)
-                ->process($request);
+                ->process($this->cleanupRequest($request));
         }
 
         return $handler->handle($request);
+    }
+
+    /**
+     * Removes `t3apiResource` query parameter as it may break further functionality.
+     * This parameter is needed only to reach a handler - further processing should not rely on it.
+     * @param ServerRequestInterface $request
+     * @return ServerRequestInterface
+     */
+    private function cleanupRequest(ServerRequestInterface $request): ServerRequestInterface
+    {
+        $cleanedQueryParams = $request->getQueryParams();
+        unset($cleanedQueryParams['t3apiResource']);
+
+        return $request->withQueryParams($cleanedQueryParams);
     }
 }
