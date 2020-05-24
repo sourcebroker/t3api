a48f047b38056a243f1da6e3b6d044ed8f97d31d
Karol Lamparski <klamparski@gmail.com>
2020-05-24 15:08:23 +0200

    Creates `UidFilter` which should be used to filter items by `uid` property in multilanguage applications

---
+++ Classes/Filter/UidFilter.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+namespace SourceBroker\T3api\Filter;
+
+use SourceBroker\T3api\Domain\Model\ApiFilter;
+use TYPO3\CMS\Extbase\Persistence\Generic\Qom\ConstraintInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
+
+class UidFilter extends NumericFilter
+{
+    public function filterProperty(
+        string $property,
+        $values,
+        QueryInterface $query,
+        ApiFilter $apiFilter
+    ): ?ConstraintInterface {
+        $query->getQuerySettings()->setRespectSysLanguage(false);
+        $query->getQuerySettings()->setLanguageOverlayMode(true);
+
+        return parent::filterProperty($property, $values, $query, $apiFilter);
+    }
+}
