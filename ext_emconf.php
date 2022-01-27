4b32e6ff45498ade4e71192239c4a86b5ca51506
Karol Lamparski <klamparski@gmail.com>
2022-01-27 14:57:07 +0100

    Upgrades supported TYPO3 version in ext conf
    #48

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.2.0-7.4.99',
-            'typo3' => '8.7.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
