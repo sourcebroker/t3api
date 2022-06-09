9a48d1ffb36eeb3e3077853ce87a5e139aa40013
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2022-06-09 22:59:28 +0200

    Update testing stack.

--- Tests/Unit/Domain/Model/ApiFilterTest.php
+++ Tests/Unit/Domain/Model/ApiFilterTest.php
@@ -15,13 +15,13 @@ class ApiFilterTest extends UnitTestCase
      */
     protected $subject = null;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
         $this->subject = new ApiFilter('filterClass', 'property', 'strategy', ['argument' => 'argumentValue']);
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
         parent::tearDown();
     }
