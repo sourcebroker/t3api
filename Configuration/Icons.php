f22baa4317f617cfc7392fba9bd95f5e0fe6dd55
Maik Schneider <mas@xima.de>
2024-03-11 08:43:39 +0100

    Register module icon for v12+

---
+++ Configuration/Icons.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'extension-t3api' => [
+        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'source' => 'EXT:t3api/Resources/Public/Icons/Extension.svg',
+    ],
+];
