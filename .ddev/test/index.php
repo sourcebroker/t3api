737a38145c9dd0a968795168d4abb0f4e8aa995b
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2024-05-30 19:49:28 +0200

    Refactor test stack.

---
+++ .ddev/test/index.php
@@ -0,0 +1,23 @@
+<?php
+$extensionKey = getenv('EXTENSION_KEY');
+$typo3AdminUser = getenv('TYPO3_INSTALL_ADMIN_USER');
+$typo3AdminPassword = getenv('TYPO3_INSTALL_ADMIN_PASSWORD');
+$supportedVersions = explode(' ', getenv('TYPO3_VERSIONS'));
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo($extensionKey); ?></title>
+</head>
+<body>
+<p>Run <b>'ddev install-all'</b> to install testing instances below.</p>
+<ul>
+    <?php
+    foreach ($supportedVersions as $version) {
+        echo "<li><a target='_blank' href='https://{$version}.{$extensionKey}.ddev.site/typo3/'>https://{$version}.{$extensionKey}.ddev.site/typo3</a> user: <b>{$typo3AdminUser}</b>, pass: <b>{$typo3AdminPassword}</b></li>";
+    }
+    ?>
+</ul>
+</body>
+</html>
--- Tests/Postman/run.sh
+++ Tests/Postman/run.sh
@@ -1,31 +1,39 @@
-TEXT_RED=$(tput setaf 1)
-TEXT_RESET=$(tput sgr0)
+#!/bin/bash
+
+source .ddev/test/utils.sh
+
 POSTMAN_TEST_PATH=/var/www/html/./Tests/Postman/
 
-if [[ ! -e "${POSTMAN_TEST_PATH}" ]]; then echo "${TEXT_RED}No POSTMAN_TEST_PATH folder: ${POSTMAN_TEST_PATH}${TEXT_RESET}" && exit 1; fi;
-cd ${POSTMAN_TEST_PATH}
+if [[ ! -e "${POSTMAN_TEST_PATH}" ]]; then
+    echo_red "No POSTMAN_TEST_PATH folder: ${POSTMAN_TEST_PATH}" && exit 1
+fi
 
-if [[ ! -e .nvmrc ]]; then echo "${TEXT_RED}No file .nvmrc with node version in folder: ${POSTMAN_TEST_PATH}${TEXT_RESET}" && exit 1; fi;
+cd ${POSTMAN_TEST_PATH} || exit
+
+if [[ ! -e ".nvmrc" ]]; then
+    echo_red "No file .nvmrc with node version in folder: ${POSTMAN_TEST_PATH}" && exit 1
+fi
 
 if [[ ! -d "node_modules" ]]; then
     npm ci
 fi
 
-POSSIBLE_ARGUMENTS=(v12 v13)
-if [[ ! ${POSSIBLE_ARGUMENTS[*]} =~ $1 ]]; then
-    INFO=$(printf ", %s" "${POSSIBLE_ARGUMENTS[@]}")
-    INFO=${INFO:1}
-    echo "${TEXT_RED}Error! Wrong argument. Possible arguments are:${INFO}${TEXT_RESET}."
-    exit 1
+TYPO3=${1}
+if [ -z "$TYPO3" ]; then
+    TYPO3=$("../../.Build/bin/typo3" | grep -oP 'TYPO3 CMS \K[0-9]+')
 fi
 
-VERSION=v12
-if [[ $1 = "v13" ]]; then
-    VERSION=v13
+if ! check_typo3_version "$TYPO3"; then
+   exit 1
 fi
 
-if [[ ! -d "/var/www/html/.test/$VERSION" ]]; then
-        echo "${TEXT_RED}Can not test. Install first $VERSION with command 'ddev install-$VERSION'${TEXT_RESET}"
+if [[ ! -d "/var/www/html/.test/$TYPO3" ]]; then
+        echo_red "Can not test. Install first TYPO3 $TYPO3 with command 'ddev install-$TYPO3'"
     else
-        ./node_modules/.bin/newman run t3apinews.postman_collection.json --env-var "baseUrl=https://$VERSION.t3api.ddev.site"
+        DOMAINS=("https://$TYPO3.t3api.ddev.site")
+        for DOMAIN in "${DOMAINS[@]}"; do
+            for TEST_FILE in tests/*.json; do
+                ./node_modules/.bin/newman run "$TEST_FILE" --env-var "baseUrl=$DOMAIN"
+            done
+        done
 fi
